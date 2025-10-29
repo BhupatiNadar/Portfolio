22cc5abb9ddb6bc4c853f65abc5085c5d27b01b1
BhupatiNadar <tamillana2@gmail.com>
2025-10-29 21:28:03 +0530

    Configured Vite for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',  // 👈 for GitHub Pages
-  build: {
-    outDir: 'docs',     // 👈 so output goes into docs/
-  },
+  base: '/Portfolio/',
 })
