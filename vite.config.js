a1f3e49ebe9dd8bf3e97725d9012e14ef5544d67
BhupatiNadar <tamillana2@gmail.com>
2025-10-29 20:33:11 +0530

    Rebuild portfolio for GitHub Pages with correct config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [react(), tailwindcss()],
   base: '/Portfolio/',
+  build: {
+    outDir: 'docs', // this makes Vite build directly into /docs
+  },
 })
-
